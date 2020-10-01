4e40af489fd90965f5e02d9b50d808a157bd8b93
Lee Michaeli <leeisr@gmail.com>
2020-10-01 12:50:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,7 +2,7 @@
 require "yaml"
 
 def load_library(file_path)
-  emoticons = emoticons.load_file(lib/emoticons.yml)
+  emoticons = emoticons.load_file(file_path)
   new_hash = {}
   emoticons.each do |key,calue|
     new_hash[key] = {}
