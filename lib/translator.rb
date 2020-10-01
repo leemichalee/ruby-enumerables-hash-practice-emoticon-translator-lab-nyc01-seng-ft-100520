e65735ca6690041de7b4cc7e2977508e3b81ea25
Lee Michaeli <leeisr@gmail.com>
2020-10-01 12:48:33 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,7 +6,7 @@ def load_library(file_path)
   new_hash = {}
   emoticons.each do |key,calue|
     new_hash[key] = {}
-    new_hash[key][:english] = value
+    new_hash[key][:english] = value[0]
 end
 
 def get_japanese_emoticon
